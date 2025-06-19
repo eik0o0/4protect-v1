c7f6ea621ff0471e2216a26ed89ed82f08746c3f
eik0o0 <140654221+eik0o0@users.noreply.github.com>
2025-06-19 22:24:25 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
-    }
-}
