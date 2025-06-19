5b1669152c83344062510501124a904113024ee5
eik0o0 <140654221+eik0o0@users.noreply.github.com>
2025-06-19 22:27:03 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bot: {
+        prefixe: '+', // Préfixe - Le texte avant les commandes
+        buyer: '621327594524114964', // Buyer
+        couleur: '#6495ED', // Couleur HEX
+        footer: 'Azuki', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+    }
+}
